c4894395a100229b27699fdcd4008173e7549201
Kano <kano@pclip.dev>
2021-07-01 11:43:21 -0700

    Debugged signup.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -3,7 +3,7 @@
 $serverName = "localhost";
 $dbUsername = "root";
 $dbPassword = "";
-$dbName = "pclip.web-account-system";
+$dbName = "test-account-system";
 
 $conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName);
 
